e798da4db0749a042448526d8212e5c4e6bd6c24
KSK OP <104455718+Rahulsharma45@users.noreply.github.com>
2022-09-23 12:27:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ get_queue = {}
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH")
 
-ASS_HANDLER = list(getenv("ASS_HANDLER", ".").split())
+ASS_HANDLER = list(getenv("ASS_HANDLER", "/").split())
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "180"))
@@ -20,8 +20,8 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/c5952790fa8235f499749.jpg")
 START_IMG = getenv("START_IMG")
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BGMIxCHATS")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", " https://t.me/KSKxCHEATS")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TheFriendGroup")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", " https://t.me/KSKxBOTS")
 
 STRING_SESSION = getenv("STRING_SESSION", None)
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "2102097596").split()))
